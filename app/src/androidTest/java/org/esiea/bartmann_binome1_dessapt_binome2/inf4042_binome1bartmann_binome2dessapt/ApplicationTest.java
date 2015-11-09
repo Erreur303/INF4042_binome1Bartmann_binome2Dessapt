ca5a032dd4c80d2960c4cba038fd61454d16f197
Ouruk <dessapt@et.esiea.fr>
2015-11-09 16:24:38 +0100

    Recreate android project
    desc Recreate android project

---
+++ app/src/androidTest/java/org/esiea/bartmann_binome1_dessapt_binome2/inf4042_binome1bartmann_binome2dessapt/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.esiea.bartmann_binome1_dessapt_binome2.inf4042_binome1bartmann_binome2dessapt;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}

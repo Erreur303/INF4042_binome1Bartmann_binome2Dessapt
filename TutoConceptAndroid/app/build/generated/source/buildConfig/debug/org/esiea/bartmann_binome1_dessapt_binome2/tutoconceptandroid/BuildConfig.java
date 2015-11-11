2777acfa9fc42523a69e00a3dd23dd180340b621
Ouruk <dessapt@et.esiea.fr>
2015-11-11 13:52:49 +0100

    Reset init android Project
    desc Reset init android Project

---
+++ TutoConceptAndroid/app/build/generated/source/buildConfig/debug/org/esiea/bartmann_binome1_dessapt_binome2/tutoconceptandroid/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package org.esiea.bartmann_binome1_dessapt_binome2.tutoconceptandroid;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "org.esiea.bartmann_binome1_dessapt_binome2.tutoconceptandroid";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
